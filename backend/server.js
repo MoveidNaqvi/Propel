1c6011674e907d75139ea6a9071f2801c167baeb
Moveid Syed <moviednaqvi@gmail.com>
2022-12-03 13:55:01 +0000

    port change due to json-server also running on port 3000

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const router = require('./routes/contactsRoutes')
 const app = express()
 
 
+app.use(express.json())
+
 app.use('/contacts', router)
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log('Server is running')
 })
 
