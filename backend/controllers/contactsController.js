8b3cf5005d1cadb854fb8cc874e923c71a596e71
Moveid Syed <moviednaqvi@gmail.com>
2022-12-03 15:19:53 +0000

    worked on getAllContacts and addContact controllers. Now able to getAllContacts and addContact through Postman

--- backend/controllers/contactsController.js
+++ backend/controllers/contactsController.js
@@ -1,13 +1,28 @@
-const getAllContacts = (req, res) => {
-  res.json({
-    message: 'Get all contacts'
-  })
+const axios = require('axios').default;
+
+const getAllContacts = async (req, res) => {
+  const { data } = await axios.get('http://localhost:3000/contacts')
+  res.json(data)
 }
 
-const addContact = (req, res) => {
-  res.json({
-    message: 'Add a contact'
+const addContact = async (req, res) => {
+  const { first_name, last_name, phone, email } = req.body
+  if(!first_name || !last_name || !phone || !email){
+    return res.status(400).json({
+      message: 'All fields are required!'
+    })
+  }
+  const { data } = await axios.post('http://localhost:3000/contacts', {
+    first_name: first_name,
+    last_name: last_name,
+    phone: phone,
+    email: email
+  }).catch(error => {
+    res.status(503).json({
+      message: error
+    })
   })
+  res.status(201).json(data)
 }
 
 const updateContact = (req, res) => {
