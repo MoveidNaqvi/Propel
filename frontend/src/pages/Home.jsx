86f78c4b5d768a6e196b35558c00f375725d93ac
Moveid Syed <moviednaqvi@gmail.com>
2022-12-05 15:58:50 +0000

    created search bar. Now able to search through contacts by firstname or last name

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,8 @@ import { FaEdit } from "react-icons/fa";
 function Home() {
   const [contacts, setContacts] = useState();
   const [loading, setLoading] = useState(false);
-  const [deleted, setDeleted] = useState(false)
+  const [deleted, setDeleted] = useState(false);
+  const [search, setSearch] = useState("")
 
   useEffect(() => {
     const fetchContacts = async () => {
@@ -20,49 +21,69 @@ function Home() {
   }, [deleted]);
 
   const handleDelete = async (id) => {
-    setDeleted(false)
+    setDeleted(false);
     await fetch(`http://localhost:5000/contacts/${id}`, {
-      method: 'DELETE'
-    })
-    setDeleted(true)
-  }
+      method: "DELETE",
+    });
+    setDeleted(true);
+  };
 
   return (
     <>
       {loading ? (
         <SpinnerCircular color="black" secondaryColor="#777" />
       ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Phone</th>
-              <th>Email</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {contacts?.length <= 0 ? <tr><td colSpan={5}><span>No contacts found!</span></td></tr> : contacts?.map((c) => (
-              <tr key={c.id}>
-                <td>{c.first_name}</td>
-                <td>{c.last_name}</td>
-                <td>{c.phone}</td>
-                <td>{c.email}</td>
-                <td>
-                  <div className="action-btns">
-                    <button className="btn-sm">
-                      <FaEdit size={20} />
-                    </button>
-                    <button className=" btn-sm" onClick={() => handleDelete(c.id)}>
-                      <MdDeleteForever size={20} />
-                    </button>
-                  </div>
-                </td>
+        <>
+          <input type="text" className="search-bar" placeholder="Search by name" value={search} onChange={(e) => setSearch(e.target.value)} />
+          <table>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {contacts?.length <= 0 ? (
+                <tr>
+                  <td colSpan={5}>
+                    <span>No contacts found!</span>
+                  </td>
+                </tr>
+              ) : (
+                contacts?.filter((c) => {
+                  if(search == "") {
+                    return c
+                  } else if (c.first_name.toLowerCase().includes(search.toLocaleLowerCase()) || c.last_name.toLowerCase().includes(search.toLocaleLowerCase())) {
+                    return c
+                  }
+                }).map((c) => (
+                  <tr key={c.id}>
+                    <td>{c.first_name}</td>
+                    <td>{c.last_name}</td>
+                    <td>{c.phone}</td>
+                    <td>{c.email}</td>
+                    <td>
+                      <div className="action-btns">
+                        <button className="btn-sm">
+                          <FaEdit size={20} />
+                        </button>
+                        <button
+                          className=" btn-sm"
+                          onClick={() => handleDelete(c.id)}
+                        >
+                          <MdDeleteForever size={20} />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </>
       )}
     </>
   );
